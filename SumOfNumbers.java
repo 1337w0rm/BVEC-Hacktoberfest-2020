fba19fc0b47bbdc91b9ee72b0e2d08c5a3954abd
Yaashi <42653542+ym2108@users.noreply.github.com>
2020-10-16 16:05:56 +0530

    Create SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	      int n=s.nextInt();
+	    int sum=0;
+	  
+for(int i=0;i<=n;i++)
+{
+    sum+=i;
+}
+	
+	    
+	   System.out.println(sum);
+	}
+}
